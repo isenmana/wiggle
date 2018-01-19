ab8909b0027d4ac8eb83b9407d3489845126d34c
isenmana <35604957+isenmana@users.noreply.github.com>
2018-01-19 15:48:46 +0000

    Update toscrape-xpath.py
    Added wiggle URL

--- quotesbot/spiders/toscrape-xpath.py
+++ quotesbot/spiders/toscrape-xpath.py
@@ -5,7 +5,7 @@ import scrapy
 class ToScrapeSpiderXPath(scrapy.Spider):
     name = 'toscrape-xpath'
     start_urls = [
-        'http://quotes.toscrape.com/',
+        'http://wiggle.com/cycle/',
     ]
 
     def parse(self, response):
